b8f9198f1ea860980f6d56355aeea3d140fd50f3
Victor Doh Kim <tank2705@gmail.com>
2021-02-18 20:46:23 -0500

    Added Keyboard event

--- pang.py
+++ pang.py
@@ -24,9 +24,12 @@ character_width=character_size[0]
 character_height=character_size[1]
 character_x_pos=screen_width/2-character_width/2
 character_y_pos=screen_height-character_height
-
-
 pygame.display.set_caption("Pang Game")
+
+
+#destination coordinate
+to_x=0
+to_y=0
 
 #Loop for event
 running = True
@@ -34,7 +37,35 @@ while running:
     for event in pygame.event.get():        
         if event.type==pygame.QUIT:
             running=False
-    
+
+        #Key event
+        if event.type == pygame.KEYDOWN:
+            if  event.key == pygame.K_LEFT:
+                to_x -= 1 
+            elif  event.key == pygame.K_RIGHT:
+                to_x += 1 
+            elif  event.key == pygame.K_UP:
+                to_y -= 1 
+            elif  event.key == pygame.K_DOWN:
+                to_y += 1 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                to_x=0
+            elif event.key == pygame.K_UP or pygame.K_DOWN:
+                to_y=0
+
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    if character_x_pos < 0:
+        character_x_pos=0
+    elif character_x_pos > screen_width-character_width:
+        character_x_pos=screen_width-character_width
+    elif character_y_pos < 0:
+        character_y_pos=0
+    elif character_y_pos > screen_height-character_height:
+        character_y_pos=screen_height-character_height
+
     screen.blit(backgrond, (0,0)) ## screen.fill((0,0,255))
     screen.blit(character,(character_x_pos,character_y_pos))
     pygame.display.update()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+            elif  event.key == pygame.K_DOWN:
+                to_y += 1 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
